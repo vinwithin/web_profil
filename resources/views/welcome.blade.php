7b22d4703e81eec61830539507e815c09c4f0b2a
vinwithin <kelvinadinata82@gmail.com>
2023-12-26 17:51:30 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <nav class="container inline-flex flex-col lg:flex-row  justify-around max-w-full bg-slate-200 lg:items-center items-start">
+        <div class=" justify-items-start ">
+            <img src="https://ml44p8frztli.i.optimole.com/w:2000/h:746/q:mauto/f:avif/https://himasi.unja.ac.id/wp-content/uploads/2020/03/cropped-logo.png"
+                alt="" class="lg:w-44  w-96">
+        </div>
+        <div class="lg:w-auto inline-flex lg:mt float-right max-w-full">
+            <ul class="flex flex-col  lg:flex-row ms-auto ">
+                <li>
+                    <a href="" class="px-4 py-2">Beranda</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Tentang Kami</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Struktur</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Kegiatan</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Berita</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Artikel</a>
+                </li>
+                <li>
+                    <a href="" class="px-4 py-2">Galery</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
